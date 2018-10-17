73fb9ad0d4c8394f611d969079aa5b1d195bab6a
aYaremchuk <jaremchuk.andrij@gmail.com>
2018-10-17 17:44:34 +0300

    Replace should in specs to is_expected

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { should validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:title) }
 
-  it { should have_and_belong_to_many(:groups) }
-  it { should belong_to(:user) }
+  it { is_expected.to have_and_belong_to_many(:groups) }
+  it { is_expected.to belong_to(:user) }
 end
